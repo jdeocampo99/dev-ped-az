f9da610022e51bacdac61ba81a61f936ff959119
Justin De Ocampo <justindeocampo99@gmail.com>
2023-10-13 19:44:34 -0700

    Add home page toolbar + content skeleton

---
+++ frontend/src/utils/colors.ts
@@ -0,0 +1,13 @@
+export type Color = {
+    mainBlue: string;
+    lightBlue: string,
+    mediumBlue: string,
+    paper: string;
+}
+
+export const COLOR: Color = {
+    mainBlue: '#0B2545',
+    mediumBlue: '#13315C',
+    lightBlue: '#8DA9C4',
+    paper: '#EEF4ED'
+}
